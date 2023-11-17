adb030e9e01da466b057d904da773bf1e1851876
Pedro Santos <pedro.santos@tmaior.com.br>
2023-11-17 11:27:45 -0300

    Implement argocd application

--- terraform/main.tf
+++ terraform/main.tf
@@ -17,22 +17,78 @@ resource "helm_release" "argocd" {
 }
 
 
+########################################################################
+#
+#                     Project -> Apps of Apps
+#
+########################################################################
+resource "kubectl_manifest" "AppProject" {
+    yaml_body = <<YAML
+apiVersion: argoproj.io/v1alpha1
+kind: AppProject
+metadata:
+  name: main-project
+  namespace: argocd
+  finalizers:
+    - resources-finalizer.argocd.argoproj.io
+spec:
+  # Project description
+  description: Testing project
 
-# resource "kubectl_manifest" "AppProject" {
-#     yaml_body = <<YAML
-# apiVersion: argoproj.io/v1alpha1
-# kind: AppProject
-# metadata:
-#   name: my-project
-#   namespace: argocd
-# spec:
-#   roles:
-#   # A role which provides read-only access to all applications in the project
-#   - name: read-only
-#     description: Read-only privileges to my-project
-#     policies:
-#     - p, proj:my-project:read-only, applications, get, my-project/*, allow
-#     groups:
-#     - my-oidc-group
-# YAML
-# }
+  sourceRepos:
+  - '*'
+
+  roles:
+  - name: read-only
+    description: Read-only privileges to main-project
+    policies:
+    - p, proj:main-project:read-only, applications, get, main-project/*, allow
+    groups:
+    - my-oidc-group
+YAML
+}
+
+########################################################################
+#
+#                    Apps of Apps
+#
+########################################################################
+resource "kubectl_manifest" "AppsOfApps" {
+    yaml_body = <<YAML
+apiVersion: argoproj.io/v1alpha1
+kind: Application
+metadata:
+  name: allApps
+  namespace: argocd
+  finalizers:
+    - resources-finalizer.argocd.argoproj.io
+  labels:
+    name:  allApps
+spec:
+  project: main-project
+  source:
+    repoURL: https://github.com/PedroAlvesBS/infra-test-argocd.git
+    targetRevision: HEAD
+    path: argo-cd
+  destination:
+    server: https://kubernetes.default.svc
+    namespace: argocd
+  syncPolicy:
+    automated:
+      prune: true
+      selfHeal: true
+    syncOptions:
+    - Validate=true
+    - CreateNamespace=true 
+    - PruneLast=true
+    managedNamespaceMetadata:
+      labels:
+        managed: argocd
+    retry:
+      limit: 5
+      backoff:
+        duration: 5s
+        factor: 2
+        maxDuration: 3m
+YAML
+}
